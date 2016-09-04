8334b836c0bfd614ac0d369d7126fd835965c15e
雨天 <77202714@qq.com>
2016-09-04 11:27:03 +0800

    added rotation

--- Assets/Scripts/Actor/FollowPlayer.cs
+++ Assets/Scripts/Actor/FollowPlayer.cs
@@ -11,10 +11,14 @@ namespace Game.Actor
         public float maxZoomDistance = 20;
         public float minZoomDistance = 8;
         public float zoomSpeed = 1;
+        public float rotateSpeed = 2;
+
+        private bool isRotating;
 
         void Awake()
         {
             player = GameObject.FindGameObjectWithTag(Tags.Player).transform;
+            transform.LookAt(player.position);
             offsetPosition = transform.position - player.position;
         }
 
@@ -22,6 +26,7 @@ namespace Game.Actor
         {
             transform.position = offsetPosition + player.position;
 
+            RotateView();
             ZoomInOut();
         }
 
@@ -32,6 +37,38 @@ namespace Game.Actor
             distance += delta;
             distance = Mathf.Clamp(distance, minZoomDistance, maxZoomDistance);
             offsetPosition = offsetPosition.normalized * distance;
+        }
+
+
+        void RotateView()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                isRotating = true;
+            }
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                isRotating = false;
+            }
+
+            if (isRotating)
+            {
+                transform.RotateAround(player.position, player.up, rotateSpeed * Input.GetAxis("Mouse X"));
+
+                Vector3 originalPos = transform.position;
+                Quaternion originalRotation = transform.rotation;
+
+                transform.RotateAround(player.position, transform.right, -rotateSpeed * Input.GetAxis("Mouse Y"));//影响的属性有两个 一个是position 一个是rotation
+                float x = transform.eulerAngles.x;
+                if (x < 10 || x > 80)
+                {
+                    transform.position = originalPos;
+                    transform.rotation = originalRotation;
+                }
+
+                offsetPosition = transform.position - player.position;
+            }
         }
     }
 }
