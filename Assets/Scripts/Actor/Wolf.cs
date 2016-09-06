87cddd36de395f787c38f8b462be0753b8e56e19
雨天 <77202714@qq.com>
2016-09-07 00:25:18 +0800

    added be attacked of wolf

--- Assets/Scripts/Actor/Wolf.cs
+++ Assets/Scripts/Actor/Wolf.cs
@@ -7,6 +7,14 @@ namespace Game.Actor
     {
         public WolfState state = WolfState.Idle;
 
+        public int hp = 100;
+        public float miss_rate = 0.2f;
+        public float redTime = 1f;
+
+        private float attackTimer = 0f;
+        private Color normalColor;
+
+
         public string animIdleKey;
         public string animWalkKey;
         public string animDeathKey;
@@ -19,12 +27,17 @@ namespace Game.Actor
 
         private Animation animPlayer;
         private CharacterController controller;
+        private bool isAttack;
+
+        private Renderer meRender;
 
         private void Awake()
         {
             animPlayer = GetComponent<Animation>();
             controller = GetComponent<CharacterController>();
             animNow = animIdleKey;
+            meRender = GetComponentInChildren<Renderer>();
+            normalColor = meRender.material.color;
         }
 
         private void Update()
@@ -41,13 +54,16 @@ namespace Game.Actor
                     if (animNow == animWalkKey)
                         controller.SimpleMove(transform.forward * speed);
                     timer += Time.deltaTime;
-                    if(timer >= timeChangeState)
+                    if (timer >= timeChangeState)
                     {
                         timer = 0;
                         RandomState();
                     }
                     break;
             }
+
+            if (Input.GetKeyDown(KeyCode.A))
+                TakeDamage(20);
         }
 
         private void RandomState()
@@ -68,5 +84,32 @@ namespace Game.Actor
             }
         }
 
+
+        public void TakeDamage(int attack)
+        {
+            float p = Random.Range(0f, 1f);
+
+            if (p < miss_rate)
+            {
+                Debug.LogWarning("Oops, miss attack: " + attack);
+            }
+            else
+            {
+                this.hp -= attack;
+                StartCoroutine(ShowBodyRed());
+                if (hp < 0)
+                {
+                    state = WolfState.Death;
+                    Destroy(this.gameObject, 2);
+                }
+            }
+        }
+
+        private IEnumerator ShowBodyRed()
+        {
+            meRender.material.color = Color.red;
+            yield return new WaitForSeconds(1f);
+            meRender.material.color = normalColor;
+        }
     }
 }
